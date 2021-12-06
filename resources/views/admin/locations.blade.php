a9ac8f670c5f330e488977e7108dcec5e10c69d0
Abiola Mishael <woroumishael@gmail.com>
2021-12-06 11:57:48 +0100

    location model

---
+++ resources/views/admin/locations.blade.php
@@ -0,0 +1,27 @@
+@extends('admin.layouts.app')
+@section('content')
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+            <div class="row page-titles mx-0">
+                <div class="col p-md-0">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Pickup/Delivery Locations</a></li>
+                    </ol>
+                </div>
+            </div>
+            <!-- row -->
+
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+@endsection
+@section('custom-script')
+    <script src="admins/plugins/validation/jquery.validate.min.js"></script>
+    <script src="admins/plugins/validation/jquery.validate-init.js"></script>
+@endsection
