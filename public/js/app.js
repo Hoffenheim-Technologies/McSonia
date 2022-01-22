f242ddc513350442c6ebab65e480d2a473db7d6c
femidamilola <femi.adebayo1998@gmail.com>
2022-01-22 07:36:05 +0100

    Updates

--- public/js/app.js
+++ public/js/app.js
@@ -2098,7 +2098,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "6318b08f48b8edb19eaa",
+  key: "",
   cluster: "mt1",
   forceTLS: true
 });
